f5ef7aa632e53a01e5b3f693a48763792fc7a0ce
vic <vicbruce@163.com>
2019-09-08 19:29:50 +0800

    xiagnmuchushihua

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    hhh
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+
+    
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
